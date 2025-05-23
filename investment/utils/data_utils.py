faf3ec22fd23d3f18429d5ecf04f36d4bac54e5e
filippobounous <filibounous@gmail.com>
2025-05-23 23:35:14 +0200

    feat(data_utils)

---
+++ investment/utils/data_utils.py
@@ -0,0 +1,6 @@
+from ..datasource.base import update_all_securities
+from ..datasource.registry import all_data_source
+
+def update_all_datasource() -> None:
+    for ds in all_data_source:
+        ds().update_all_securities(intraday=False)
