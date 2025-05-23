8f91bc655057e210fbc6e9fadb054ca489c9b636
filippobounous <filibounous@gmail.com>
2025-05-23 18:09:24 +0200

    feat(alpha_vantage)

--- investment/datasource/alpha_vantage.py
+++ investment/datasource/alpha_vantage.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import requests
+from typing import TYPE_CHECKING
 
 from .base import BaseDataSource
 from ..config import ALPHA_VANTAGE_API_KEY
-from ..core.security.registry import CurrencyCross, Equity, ETF, Fund
+
+if TYPE_CHECKING:
+    from ..core.security.registry import CurrencyCross, Equity, ETF, Fund
 
 # https://alpha-vantage.readthedocs.io/en/latest/
 # https://www.alphavantage.co/documentation/
@@ -13,10 +16,11 @@ class AlphaVantageDataSource(BaseDataSource):
     name: str = "alpha_vantage"
     base_url: str = "https://www.alphavantage.co/query"
 
-    def _get_currency_cross_ts_from_remote(self, security: CurrencyCross, intraday: bool) -> pd.DataFrame:
+    def _get_currency_cross_ts_from_remote(self, security: 'CurrencyCross', intraday: bool) -> pd.DataFrame:
+        params = {}
         if intraday:
             function_param = "FX_INTRADAY"
-            params = {"interval": "1min"}
+            params.update({"interval": "1min"})
         else:
             function_param = "FX_DAILY"
         
@@ -33,15 +37,16 @@ class AlphaVantageDataSource(BaseDataSource):
 
         return pd.DataFrame(data)
 
-    def _get_equity_ts_from_remote(self, security: Equity, intraday: bool) -> pd.DataFrame:
+    def _get_equity_ts_from_remote(self, security: 'Equity', intraday: bool) -> pd.DataFrame:
+        params = {}
         if intraday:
             function_param = "TIME_SERIES_INTRADAY"
-            params = {
+            params.update({
                 "interval": "1min",
                 "symbol": security.alpha_vantage_code,
                 "adjusted": True,
                 "extended_hours": True,
-            }
+            })
         else:
             function_param = "TIME_SERIES_DAILY_ADJUSTED"
         
@@ -57,8 +62,12 @@ class AlphaVantageDataSource(BaseDataSource):
 
         return pd.DataFrame(data)
 
-    def _get_etf_ts_from_remote(self, security: ETF, intraday: bool) -> pd.DataFrame:
-        return NotImplementedError("Method not implemented.")
+    def _get_etf_ts_from_remote(self, security: 'ETF', intraday: bool) -> pd.DataFrame:
+        raise NotImplementedError("Method not implemented.")
 
-    def _get_fund_ts_from_remote(self, security: Fund, intraday: bool) -> pd.DataFrame:
-        return NotImplementedError("Method not implemented.")
+    def _get_fund_ts_from_remote(self, security: 'Fund', intraday: bool) -> pd.DataFrame:
+        raise NotImplementedError("Method not implemented.")
+    
+    @staticmethod
+    def _format_ts_from_remote(df: pd.DataFrame) -> pd.DataFrame:
+        pass # TODO
