2217fcbbdee75d25baf4cb6ce18119951f9547e7
filippobounous <filibounous@gmail.com>
2025-05-23 22:45:26 +0200

    feat(YahooFinanceDataSource)

--- investment/datasource/yahoo_finance.py
+++ investment/datasource/yahoo_finance.py
@@ -1,3 +1,4 @@
+import datetime
 import pandas as pd
 from typing import TYPE_CHECKING, ClassVar
 import yfinance as yf
@@ -7,15 +8,68 @@ from ..utils.consts import DATA_START_DATE
 from ..utils.date_utils import today_midnight
 
 if TYPE_CHECKING:
-    from ..core import Security
+    from ..core.security.registry import CurrencyCross, Equity, ETF, Fund
+
+# https://ranaroussi.github.io/yfinance/
 
 class YahooFinanceDataSource(BaseDataSource):
     name: ClassVar[str] = "yahoo_finance"
 
-    def _get_ts_from_remote(self, security: 'Security') -> pd.DataFrame:
-        ticker = yf.Ticker(security.yahoo_finance_code)
-        df = ticker.history()
-        return df
+    def _get_currency_cross_ts_from_remote(
+        self,
+        security: 'CurrencyCross', intraday: bool,
+        start_date: datetime.datetime, end_date: datetime.datetime,
+    ) -> pd.DataFrame:
+        symbol = security.yahoo_finance_code
+        return self._time_series(
+            symbol=symbol, intraday=intraday,
+            start_date=start_date, end_date=end_date,
+        )
+
+    def _get_equity_ts_from_remote(
+        self,
+        security: 'Equity', intraday: bool,
+        start_date: datetime.datetime, end_date: datetime.datetime,
+    ) -> pd.DataFrame:
+        raise NotImplementedError("Method not implemented.")
+
+    def _get_etf_ts_from_remote(
+        self,
+        security: 'ETF', intraday: bool,
+        start_date: datetime.datetime, end_date: datetime.datetime,
+    ) -> pd.DataFrame:
+        raise NotImplementedError("Method not implemented.")
+
+    def _get_fund_ts_from_remote(
+        self,
+        security: 'Fund', intraday: bool,
+        start_date: datetime.datetime, end_date: datetime.datetime,
+    ) -> pd.DataFrame:
+        raise NotImplementedError("Method not implemented.")
     
-    def update_all_securities(self):
-        pass
+    @staticmethod
+    def _format_ts_from_remote(df: pd.DataFrame) -> pd.DataFrame:
+        df_simple = df.copy().reset_index()
+        df_simple.columns = [
+            col.lower()
+            for col in df_simple.columns.get_level_values(0)
+        ]
+        df_simple = df_simple.rename(columns={"date": "as_of_date"})
+        return df_simple
+        
+    def _time_series(
+        self,
+        symbol: str, intraday: bool,
+        start_date: datetime.datetime, end_date: datetime.datetime,
+    ) -> pd.DataFrame:
+        interval =  "1m" if intraday else "1d"
+
+        end_date = end_date + datetime.timedelta(days=2) # required by API
+        
+        return yf.download(
+            symbol,
+            interval=interval,
+            start=start_date.strftime("%Y-%m-%d"),
+            end=end_date.strftime("%Y-%m-%d"),
+            auto_adjust=True,
+        )
