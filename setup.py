7f05162a7153f00dd80790081ef5ef54b3cb9967
filippobounous <filibounous@gmail.com>
2025-05-23 11:13:45 +0200

    fix(setup.py)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="investment",
     version="0.1.0",
-    packages=find_packages(exclude=("notebooks", "private")),
+    packages=find_packages(exclude=("notebooks")),
     install_requires=[
         "pandas",
         "pydantic",
