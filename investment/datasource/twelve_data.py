49b21f44d8881fd0b18380a8116773bad3447abf
filippobounous <filibounous@gmail.com>
2025-05-23 22:08:29 +0200

    fix(datasource for fx): twelve_data

--- investment/datasource/twelve_data.py
+++ investment/datasource/twelve_data.py
@@ -1,44 +1,57 @@
+import datetime
 import pandas as pd
+from pydantic import ConfigDict
 import requests
-from typing import TYPE_CHECKING, List, Dict, Union
 from twelvedata import TDClient
+from typing import TYPE_CHECKING, List, Dict, Union, ClassVar, Tuple, Any
 
 from .base import BaseDataSource
 from ..config import TWELVE_DATA_API_KEY
-from ..core.security.currency_cross import CurrencyCross
 
 if TYPE_CHECKING:
-    from ..core import Security
+    from ..core.security.registry import CurrencyCross, Equity, ETF, Fund
 
 # https://github.com/twelvedata/twelvedata-python
 
 class TwelveDataDataSource(BaseDataSource):
-    name: str = "twelve_data"
-    allowed_intervals: List[str] = [
-        '1min', '5min', '15min', '30min', '45min',
-        '1h', '2h', '4h', '8h', '1day',
-        '1week', '1month'
-    ]
+    name: ClassVar[str] = "twelve_data"
+    td: TDClient = TDClient(apikey=TWELVE_DATA_API_KEY)
+    output_size: int = 5000
+    data_start_date: datetime.datetime = datetime.datetime(2003,12,1)
 
-    def _get_ts_from_remote(self, security: "Security", interval: str = '1day') -> pd.DataFrame:
-        if isinstance(security, CurrencyCross):
-            symbol = f"{security.currency_vs}/{security.currency}"
-        else:
-            symbol = self._get_symbol(isin_code=security.code)
+    model_config = ConfigDict(arbitrary_types_allowed=True)
 
-        if symbol is None:
-            raise ValueError(f"Security {security.code} not found in {self.name} data source.")
+    def _get_currency_cross_ts_from_remote(
+        self,
+        security: 'CurrencyCross', intraday: bool,
+        start_date: datetime.datetime, end_date: datetime.datetime,
+    ) -> pd.DataFrame:
+        symbol = f"{security.currency_vs}/{security.currency}"
+        return self._time_series(
+            symbol=symbol, intraday=intraday,
+            start_date=start_date, end_date=end_date,
+        )
 
-        td = TDClient(apikey=TWELVE_DATA_API_KEY)
-        
-        if interval in self.allowed_intervals:
-            df = td.time_series(
-                symbol=symbol,
-                interval=interval,
-            ).as_pandas()
-            return df
-        else:
-            raise ValueError(f"Interval {interval} not acceptable, please pass one of {self.allowed_intervals}.")
+    def _get_equity_ts_from_remote(
+        self,
+        security: 'Equity', intraday: bool,
+        start_date: datetime.datetime, end_date: datetime.datetime,
+    ) -> pd.DataFrame:
+        raise NotImplementedError("Method not implemented.")
+
+    def _get_etf_ts_from_remote(
+        self,
+        security: 'ETF', intraday: bool,
+        start_date: datetime.datetime, end_date: datetime.datetime,
+    ) -> pd.DataFrame:
+        raise NotImplementedError("Method not implemented.")
+
+    def _get_fund_ts_from_remote(
+        self,
+        security: 'Fund', intraday: bool,
+        start_date: datetime.datetime, end_date: datetime.datetime,
+    ) -> pd.DataFrame:
+        raise NotImplementedError("Method not implemented.")
     
     @staticmethod
     def _get_symbol(isin_code: str) -> str:
@@ -49,3 +62,57 @@ class TwelveDataDataSource(BaseDataSource):
         
         if len(li) > 0:
             return li[0].get("symbol")
+    
+    @staticmethod
+    def _format_ts_from_remote(df: pd.DataFrame) -> pd.DataFrame:
+        return df.reset_index().rename(columns={"datetime":"as_of_date"})
+    
+    def _get_dates(
+        self,
+        start_date: datetime.datetime,
+        end_date: datetime.datetime,
+        intraday: bool
+    ) -> List[Tuple[datetime.datetime, datetime.datetime]]:
+        result = []
+        current_start = start_date
+        shifted_end_date = end_date + datetime.timedelta(days=2) # required by API
+
+        delta = (
+            timedelta(minutes=self.output_size)
+            if intraday
+            else datetime.timedelta(days=self.output_size - 1)
+        )
+
+        while current_start <= shifted_end_date:
+            current_end = min(
+                current_start + delta,
+                shifted_end_date,
+            )
+            result.append((current_start, current_end))
+            current_start = current_end + datetime.timedelta(days=1)
+
+        return result
+        
+    def _time_series(
+        self,
+        symbol: str, intraday: bool,
+        start_date: datetime.datetime, end_date: datetime.datetime,
+    ) -> pd.DataFrame:
+        interval =  "1min" if intraday else "1day"
+        
+        dfs = []
+        dates = self._get_dates(start_date=start_date, end_date=end_date, intraday=intraday)
+
+        for _start_date, _end_date in dates:
+            dfs.append(self.td.time_series(
+                symbol=symbol,
+                interval=interval,
+                outputsize=self.output_size,
+                start_date=_start_date.strftime("%Y-%m-%d"),
+                end_date=_end_date.strftime("%Y-%m-%d"),
+            ).as_pandas())
+
+        return pd.concat(dfs)
+    
+    def usage(self) -> Dict[str, Any]:
+        return self.td.api_usage().as_json()
