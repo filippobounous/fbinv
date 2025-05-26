47b2c5715ec696f91fb40b65cd630f7ddfc2017b
filippobounous <filibounous@gmail.com>
2025-05-26 17:33:51 +0200

    feat(data_utils)

--- investment/utils/data_utils.py
+++ investment/utils/data_utils.py
@@ -9,3 +9,10 @@ def update_all_datasource() -> None:
     for ds in all_data_source:
         ds_instance: BaseDataSource = ds()
         ds_instance.update_all_securities(intraday=False)
+
+def update_security_mapping() -> None:
+    LocalDataSource().update_security_mappings()
+
+def update_all() -> None:
+    update_security_mapping()
+    update_all_datasource()
