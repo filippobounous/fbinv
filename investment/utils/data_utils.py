4eede0a46259f120cadeef122b81e048300c8b8a
filippobounous <filibounous@gmail.com>
2025-05-26 17:27:56 +0200

    fix(update_all_datasource)

--- investment/utils/data_utils.py
+++ investment/utils/data_utils.py
@@ -1,5 +1,11 @@
-from ..datasource.registry import all_data_source
+from typing import TYPE_CHECKING
+
+from ..datasource.registry import all_data_source, LocalDataSource
+
+if TYPE_CHECKING:
+    from ..datasource.base import BaseDataSource
 
 def update_all_datasource() -> None:
     for ds in all_data_source:
-        ds().update_all_securities(intraday=False)
+        ds_instance: BaseDataSource = ds()
+        ds_instance.update_all_securities(intraday=False)
