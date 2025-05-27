852f49fe5071434357cd4eae33343f26f7584bdd
filippobounous <filibounous@gmail.com>
2025-05-27 09:08:51 +0200

    fix(data_utils)

--- investment/utils/data_utils.py
+++ investment/utils/data_utils.py
@@ -1,9 +1,6 @@
 from typing import TYPE_CHECKING
 
-from ..datasource.registry import all_data_source, LocalDataSource
-
-if TYPE_CHECKING:
-    from ..datasource.base import BaseDataSource
+from ..datasource.registry import all_data_source, BaseDataSource
 
 def update_all_datasource() -> None:
     for ds in all_data_source:
@@ -11,7 +8,9 @@ def update_all_datasource() -> None:
         ds_instance.update_all_securities(intraday=False)
 
 def update_security_mapping() -> None:
-    LocalDataSource().update_security_mappings()
+    for ds in all_data_source:
+        ds_instance: BaseDataSource = ds()
+        ds_instance.update_security_mappings()
 
 def update_all() -> None:
     update_security_mapping()
