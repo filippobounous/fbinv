0d19416e5f34c1ee36de0af2494986d29eac6363
filippobounous <filibounous@gmail.com>
2025-05-27 11:50:58 +0200

    fix(security base)

--- investment/core/security/base.py
+++ investment/core/security/base.py
@@ -34,6 +34,7 @@ class Security(BaseMappingEntity):
             raise ValueError(f"{self.code} is missing required values for: {missing}.")
 
     def get_file_path(self, datasource_name: str, intraday: bool) -> str:
-        file_name = getattr(self, f"{datasource_name}_code", None)
+        _file_name = getattr(self, f"{datasource_name}_code", None)
+        file_name = _file_name.replace("/", "")
         folder_name = "intraday" if intraday else "daily"
         return f"{HISTORICAL_DATA_PATH}/{folder_name}/{datasource_name}/{self.entity_type}/{file_name}_{folder_name}.csv"
