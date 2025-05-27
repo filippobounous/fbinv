69af7ab7b776de5f286f014b9fe2b5d5934e98d1
filippobounous <filibounous@gmail.com>
2025-05-27 20:25:51 +0200

    fix(error handling)

--- investment/core/security/base.py
+++ investment/core/security/base.py
@@ -42,8 +42,8 @@ class Security(BaseMappingEntity):
             _file_name = "figi_code"
         else:
             _file_name = getattr(self, f"{datasource_name}_code", None)
-
-        file_name = _file_name.replace("/", "")
+        
+        file_name = _file_name.replace("/", "") if _file_name else _file_name
 
         folder_name = "intraday" if intraday else "daily"
         return f"{HISTORICAL_DATA_PATH}/{folder_name}/{datasource_name}/{self.entity_type}/{file_name}_{folder_name}.csv"
