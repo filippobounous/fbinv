fe9a28830c962bcc1620a9fd6cd46ef8b5ae6e48
filippobounous <filibounous@gmail.com>
2025-05-27 09:46:44 +0200

    fix(data_utils)

--- investment/utils/data_utils.py
+++ investment/utils/data_utils.py
@@ -1,3 +1,4 @@
+from tqdm import tqdm
 from typing import TYPE_CHECKING
 
 from ..datasource.registry import all_data_source
@@ -6,12 +7,12 @@ if TYPE_CHECKING:
     from ..datasource.base import BaseDataSource
 
 def update_all_securities() -> None:
-    for ds in all_data_source:
+    for ds in tqdm(all_data_source, desc="Updating all data sources"):
         ds_instance: "BaseDataSource" = ds()
         ds_instance.update_all_securities(intraday=False)
 
 def update_security_mapping() -> None:
-    for ds in all_data_source:
+    for ds in tqdm(all_data_source, desc="Updating all security mappings"):
         ds_instance: "BaseDataSource" = ds()
         ds_instance.update_security_mappings()
 
