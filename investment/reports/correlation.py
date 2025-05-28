c23be88c46e5b0ff5edf456a428ccf6a2d0b7b44
filippobounous <filibounous@gmail.com>
2025-05-28 22:18:49 +0200

    feat(correlation)

--- investment/reports/correlation.py
+++ investment/reports/correlation.py
@@ -1,2 +1,11 @@
+from typing import Optional, List, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ..core import Security, Portfolio
 class Correlation:
-    pass
+    def __init__(
+        self,
+        securities: Optional[List["Security"]] = None,
+        portfolios: Optional[List["Portfolio"]] = None,
+    ) -> None:
+        pass
