fff0a4cd655845ac748c00542539792a72469bae
filippobounous <filibounous@gmail.com>
2025-05-28 17:44:31 +0200

    feat(data_utils)

--- investment/utils/data_utils.py
+++ investment/utils/data_utils.py
@@ -6,11 +6,11 @@ from ..datasource.registry import all_datasource
 if TYPE_CHECKING:
     from ..datasource.base import BaseDataSource
 
-def update_all_timeseries() -> Dict[str, Dict[str, bool]]:
+def update_all_timeseries(intraday: bool = False, local_only: bool = False) -> Dict[str, Dict[str, bool]]:
     di = {}
     for ds in tqdm(all_datasource, desc="Updating all data sources"):
         ds_instance: "BaseDataSource" = ds()
-        di[ds_instance.name] = ds_instance.update_all_timeseries(intraday=False)
+        di[ds_instance.name] = ds_instance.update_all_timeseries(intraday=intraday, local_only=local_only)
     return di
 
 def update_security_mapping() -> Dict[str, bool]:
@@ -20,7 +20,7 @@ def update_security_mapping() -> Dict[str, bool]:
         di[ds_instance.name] = False if ds_instance.update_security_mapping().empty else True
     return di
 
-def update_all() -> Tuple[Dict[str, bool], Dict[str, Dict[str, bool]]]:
+def update_all(intraday: bool = False, local_only: bool = False) -> Tuple[Dict[str, bool], Dict[str, Dict[str, bool]]]:
     di_mapping = update_security_mapping()
-    di_timeseries = update_all_timeseries()
+    di_timeseries = update_all_timeseries(intraday=intraday, local_only=local_only)
     return di_mapping, di_timeseries
