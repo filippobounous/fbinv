5bf03d6ad6544000410b05b58d2484220975f50d
filippobounous <filibounous@gmail.com>
2025-05-28 19:07:49 +0200

    fix(ReturnsCalculator)

--- investment/analytics/returns.py
+++ investment/analytics/returns.py
@@ -3,28 +3,13 @@ import numpy as np
 from typing import Union, List
 
 class ReturnsCalculator:
-    def __init__(self, ln_ret: bool = True, ret: bool = False, ret_win_size: List[int] = [1, 20, 60, 120, 252]):
-        self.ln_ret = ln_ret
-        self.ret = ret
-        self.ret_win_size = ret_win_size
+    def __init__(self, use_ln_ret: bool = True, ret_win_size: Union[int, List[int]] = [1, 20, 60, 120, 252]):
+        self.use_ln_ret = use_ln_ret
 
-    @staticmethod
-    def format_col_name(ln_ret: bool, ret: bool, ret_win_size: Union[int, List[int]]) -> List[str]:
-        """
-        Generate column names for returns based on the return type flags and window size(s).
-        """
         if isinstance(ret_win_size, int):
-            ret_win_size = [ret_win_size]
-
-        col_names = []
-
-        for n in ret_win_size:
-            if ret:
-                col_names.append(f'{n}d_ret')
-            if ln_ret:
-                col_names.append(f'{n}d_ln_ret')
-
-        return col_names
+            self.ret_win_size = [ret_win_size]
+        else:
+            self.ret_win_size = ret_win_size
 
     def calculate_returns(self, df: pd.DataFrame) -> pd.DataFrame:
         """
@@ -34,15 +19,22 @@ class ReturnsCalculator:
             raise ValueError("Input DataFrame is empty.")
         if 'close' not in df.columns:
             raise ValueError("DataFrame must contain a 'close' column.")
+        if not isinstance(df.index, pd.DatetimeIndex):
+            raise ValueError("DataFrame index must be a DatetimeIndex.")
 
-        result_df = df.copy()
-
+        df_list = []
         for n in self.ret_win_size:
-            cols = self.format_col_name(ln_ret=self.ln_ret, ret=self.ret, ret_win_size=n)
-            for col in cols:
-                if col.endswith("d_ret") and col not in result_df.columns:
-                    result_df[col] = result_df['close'].pct_change(n)
-                elif col.endswith("d_ln_ret") and col not in result_df.columns:
-                    result_df[col] = np.log(result_df['close'] / result_df['close'].shift(n))
+            if self.use_ln_ret:
+                rets = np.log(df['close'] / df['close'].shift(n))
+            else:
+                rets = df['close'].pct_change(n)
+            
+            temp_df = pd.DataFrame({
+                'value': rets,
+                'is_ln_ret': self.use_ln_ret,
+                'as_of_date': df.index,
+                'ret_win_size': n
+            })
+            df_list.append(temp_df)
 
-        return result_df
+        return pd.concat(df_list).dropna().reset_index(drop=True)
