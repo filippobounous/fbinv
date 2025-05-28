f450744025026e505e1a54a6636a1fe38aa5bf1f
filippobounous <filibounous@gmail.com>
2025-05-28 22:06:04 +0200

    feat(returns)

--- investment/analytics/returns.py
+++ investment/analytics/returns.py
@@ -2,8 +2,14 @@ import pandas as pd
 import numpy as np
 from typing import Union, List
 
+from ..utils.consts import DEFAULT_RET_WIN_SIZE
+
 class ReturnsCalculator:
-    def __init__(self, use_ln_ret: bool = True, ret_win_size: Union[int, List[int]] = [1, 20, 60, 120, 252]):
+    def __init__(
+            self,
+            use_ln_ret: bool = True,
+            ret_win_size: Union[int, List[int]] = DEFAULT_RET_WIN_SIZE
+        ) -> None:
         self.use_ln_ret = use_ln_ret
 
         if isinstance(ret_win_size, int):
@@ -11,12 +17,15 @@ class ReturnsCalculator:
         else:
             self.ret_win_size = ret_win_size
 
-    def calculate_returns(self, df: pd.DataFrame) -> pd.DataFrame:
+    def calculate(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         Calculate log and/or simple returns from the 'close' column for specified periods.
         """
         if df.empty:
             raise ValueError("Input DataFrame is empty.")
+        
+        df = df.sort_index()
+
         if 'close' not in df.columns:
             raise ValueError("DataFrame must contain a 'close' column.")
         if not isinstance(df.index, pd.DatetimeIndex):
@@ -33,7 +42,7 @@ class ReturnsCalculator:
                 'as_of_date': df.index,
                 'is_ln_ret': self.use_ln_ret,
                 'ret_win_size': n,
-                'value': rets,
+                'return': rets,
             })
             df_list.append(temp_df)
 
