6f7f4f403300f911b30c2ec4ded9a63cee458d58
filippobounous <filibounous@gmail.com>
2025-06-01 17:16:58 +0200

    fix(init)

--- investment/core/security/__init__.py
+++ investment/core/security/__init__.py
@@ -1,3 +1,10 @@
 "Security class and subclasses"
 
 from .base import Security
+from .composite import Composite
+from .currency_cross import CurrencyCross
+from .equity import Equity
+from .etf import ETF
+from .fund import Fund
+from .isin import ISINSecurity
+from .registry import security_registry
