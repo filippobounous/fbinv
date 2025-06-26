7f9724190fd80e85405d1f3fbb6d4497079a2ad7
Filippo Bounous <62483970+filippobounous@users.noreply.github.com>
2025-06-26 11:29:42 +0200

    Install numpy for tests

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     packages=find_packages(exclude=("notebooks")),
     install_requires=[
         "pandas",
+        "numpy",
         "pydantic",
         "yfinance",
         "twelvedata",
