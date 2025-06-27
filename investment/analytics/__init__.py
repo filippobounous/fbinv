880229067796b0d1dff82fe6f04bbcf854c75fa5
Filippo Bounous <62483970+filippobounous@users.noreply.github.com>
2025-06-28 00:01:29 +0200

    chore: address reviewer comments

--- investment/analytics/__init__.py
+++ investment/analytics/__init__.py
@@ -2,3 +2,6 @@
 
 from .realised_volatility import RealisedVolatilityCalculator
 from .returns import ReturnsCalculator
+from .monte_carlo import BaseMonteCarloEngine, PricePathEngine, VolatilityEngine
+from .random_generators import RandomGenerator
+
