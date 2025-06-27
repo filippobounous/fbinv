25125d40d95abebe9f5a4136ddb9173b9530ca5d
filippobounous <filibounous@gmail.com>
2025-06-28 00:28:07 +0200

    fix(blank)

--- investment/analytics/__init__.py
+++ investment/analytics/__init__.py
@@ -5,4 +5,3 @@ from .realised_volatility import RealisedVolatilityCalculator
 from .returns import ReturnsCalculator
 from .monte_carlo import BaseMonteCarloEngine, PricePathEngine, VolatilityEngine
 from .random_generators import RandomGenerator
-
