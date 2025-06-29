d851346540d7bbe51ae4978e136d7498d4dbaf0f
filippobounous <filibounous@gmail.com>
2025-06-29 09:07:43 +0200

    feat(metrics): todo

--- investment/analytics/metrics.py
+++ investment/analytics/metrics.py
@@ -17,6 +17,11 @@ import pandas as pd
 from .base import _BaseAnalytics
 from ..utils.consts import TRADING_DAYS
 
+# TODO: rolling window with win_size instead of setting as single value
+# TODO: implement correlation for portfolio
+# TODO: rethink how this is used in the context of portfolio etc, perhaps should be done similarly
+# to correlation page
+
 class PerformanceMetrics(_BaseAnalytics):
     """Collection of common portfolio performance calculations.
 
