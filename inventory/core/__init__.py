1ee524746ae99421c201dc122a9af299728a4321
filippobounous <filibounous@gmail.com>
2025-06-30 21:10:00 +0200

    fix(placeholder)

--- inventory/core/__init__.py
+++ inventory/core/__init__.py
@@ -1,1 +1,8 @@
 """Inventory core module and submodules"""
+
+from .house import House
+from .item import Item
+from .mapping import BaseMappingEntity
+from .room import Room
+
+__all__ = ["House", "Item", "BaseMappingEntity", "Room"]
